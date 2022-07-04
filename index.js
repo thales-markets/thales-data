e5f4ce1ba8cb4dff28799f5f6392f9709e49d107
dmikic <dusan.mikic15@gmail.com>
2022-07-04 18:15:50 +0200

    Added support for sport markets on optimism

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const graphAPIEndpoints = {
 	},
 	sportMarkets: {
 		42: 'https://api.thegraph.com/subgraphs/name/thales-markets/sport-markets-kovan', // kovan
+		10: 'https://api.thegraph.com/subgraphs/name/thales-markets/sport-markets-optimism', // optimism
 	},
 };
 
